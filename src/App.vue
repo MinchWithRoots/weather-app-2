ee9799326803dab7d5325b6005526686fe78ca02
MinchWithRoots <mashedem@gmail.com>
2024-05-04 18:12:48 +0300

    added more weather conditions

--- src/App.vue
+++ src/App.vue
@@ -28,6 +28,16 @@
         <img class="weather-bg_img overcast" src="./assets/overcast.jpg" alt="Overcast">
         <img class="weather-bg_img partly-cloudy" src="./assets/partly-cloudy.jpg" alt="Partly-cloudy">
         <img class="weather-bg_img sunny" src="./assets/sunny.jpg" alt="Sunny">
+        <!-- snow -->
+        <img class="weather-bg_img light-snow" src="./assets/light snow.jpg" alt="light snow">
+        <img class="weather-bg_img snow" src="./assets/snow.jpg" alt="snow">
+        <img class="weather-bg_img heavy-snow" src="./assets/heavy snow.jpg" alt="heavy snow">
+        <img class="weather-bg_img rain-and-snow" src="./assets/rain-and-snow.jpg" alt="rain and snow">
+        <!-- rain -->
+        <img class="weather-bg_img light-rain" src="./assets/light-rain.jpg" alt="light rain">
+        <img class="weather-bg_img moderate-rain" src="./assets/moderate-rain.jpg" alt="moderate rain">
+        <img class="weather-bg_img heavy-intensity-rain" src="./assets/heavy-intensity-rain.jpg" alt="heavy intensity rain">
+        <img class="weather-bg_img very-heavy-rain" src="./assets/very-heavy-rain.jpg" alt="very heavy rain">
       </div>
     </div>
   </div>
@@ -50,11 +60,28 @@ export default {
     weatherClass() {
       if (this.description.includes('Sunny')) {
         return 'sunny';
-     } else if (this.description.includes('Overcast')) {
+      } else if (this.description.includes('Overcast')) {
        return 'overcast';
-     } else if (this.description.includes('Partly cloudy')) {
+      } else if (this.description.includes('Partly cloudy')) {
        return 'partly-cloudy';
-     } else {
+      } else if (this.description.includes('Light snow')) {
+       return 'light snow';
+      } else if (this.description.includes('Snow')) {
+       return 'snow';
+      } else if (this.description.includes('Heavy Snow')) {
+       return 'heavy snow';
+      } else if (this.description.includes('Rain and snow')) {
+       return 'rain-and-snow';
+      } else if (this.description.includes('Light rain')) {
+       return 'light-rain';
+      } else if (this.description.includes('Moderate rain')) {
+       return 'moderate-rain';
+      } else if (this.description.includes('Heavy intensity rain')) {
+       return 'heavy-intensity-rain';
+      } else if (this.description.includes('Very heavy rain')) {
+       return 'very-heavy-rain';
+      }
+      else {
        return '';
      }
    }
